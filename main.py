855006c04352e08cb13f8a942a31e5bf1dcbcc0b
yamamoto <yamamoto.hiroto932@mail.kyutech.jp>
2024-07-19 21:02:27 +0900

    メインウィンドウを表示

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import tkinter as tk
+import openpyxl as excel
+
+from tkinter import messagebox as mbox
+
+window = tk.Tk()
+window.geometry("300x100")
+window.title("クイズ出題くん")
+window.mainloop()
